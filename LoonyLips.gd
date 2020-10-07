f69e8d63df41b30990366ab951986c02a7045d6b
YM <thehousewhite@gmail.com>
2020-10-07 22:41:17 +0200

    add more stories and randomize effect

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,17 +1,35 @@
 extends Control
 
 var player_words = []
-var prompts = ["a name", "a noun", "adjective"]
-var story = "Once upon a time %s watched a movie called %s and thought it was %s" 
-var first_time_flag = true
 
+var first_time_flag = true
+var template = [
+		{"prompts": ["a name", "a noun", "adjective"],
+		"story":"Once upon a time %s watched a movie called %s and thought it was %s"},
+		{"prompts": ["a hobby", "adverb", "verb"],
+		"story":"I know you like %s, which is like a dog eating its food %s because the dog wants to %s"},
+		{"prompts": ["a thing", "verb", "feeling"],
+		"story":"Things like %s usuaaly are hard to %s, this will make us %s"},
+		{"prompts": ["a noun", "adjective", "adverb"],
+		"story":"You have no idea of %s because it is so %s that we may have to think abou it %s"},
+		{"prompts": ["a thing", "a name", "a description word(an adjective)", "a thing"],
+		"story":"There once was %s called %s that lived as %s as a %s"}
+]
+var current_story 
 onready var player_text = $VBoxContainer/HBoxContainer/PlayerText
 onready var display_text = $VBoxContainer/DisplayText
+onready var button_label_text = $VBoxContainer/HBoxContainer/TextureButton/ButtonLabel 
 
 func _ready():
 #	#get_node("Label").text = text
-	display_text.text = "Welcome to the LoonyLips! Prese ok to start!"
-	player_text.grab_focus()
+	select_story()
+	display_text.text = "Welcome to the LoonyLips! You will give input to make a full story. Prese ok when you are ready!"
+	player_text.visible = false
+	
+	
+func select_story():
+	randomize()
+	current_story = template[randi() % template.size()]
 	
 func start_game():
 	check_player_words_length()
@@ -22,7 +40,10 @@ func _on_PlayerText_text_entered(new_text):
 
 
 func _on_TextureButton_pressed():
-	handle_player_input() 
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		handle_player_input() 
 #	update_DisplayText(player_text.text)
 #	# Replace with function body.
 
@@ -44,19 +65,28 @@ func add_to_player_words():
 	check_player_words_length()
 
 func is_story_done():
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 	
 func check_player_words_length():
 	if is_story_done():
-		tell_story()
+		end_game()
 	else:
 		prompt_player()
+		player_text.visible = true
+		player_text.grab_focus()
 
 func tell_story():
-	display_text.text = story % player_words
-	player_words.clear()
-	display_text.text += "\n (press ok to restart)"
-	first_time_flag = true
+	display_text.text = current_story.story % player_words
+	#player_words.clear()
+	#display_text.text += "\n (press ok to restart)"
+	#first_time_flag = true
 	
 func prompt_player():
-	display_text.text = 	"May I have " + prompts[player_words.size()] + " please?"
+	display_text.text = 	"May I have " + current_story.prompts[player_words.size()] + " please?"
+
+func end_game():
+	player_text.queue_free()
+	tell_story()
+	display_text.text += "\n (press ok to restart)"
+	
+	
